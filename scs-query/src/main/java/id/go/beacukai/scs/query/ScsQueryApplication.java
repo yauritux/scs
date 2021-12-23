50acae4388579d92d5c38d7c4d0a84a699963c99
Yauri Attamimi <yauritux@gmail.com>
2021-12-23 11:21:00 +0700

    Setup Query Service

---
+++ scs-query/src/main/java/id/go/beacukai/scs/query/ScsQueryApplication.java
@@ -0,0 +1,12 @@
+package id.go.beacukai.scs.query;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScsQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScsQueryApplication.class, args);
+    }
+}
